b07bce6293ab171c20e203d707862868bbe8855b
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-10-25 10:35:53 -0600

    update tf required version constraint

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -26,7 +26,7 @@ module "api_gateway" {
 }
 
 module "cloudfront_distribution" {
-  source  = "../.."
+  source = "../.."
 
   origin = {
     api_gateway_backend = {
@@ -48,7 +48,7 @@ module "cloudfront_distribution" {
 
 module "resource_names" {
   source  = "terraform.registry.launch.nttdata.com/module_library/resource_name/launch"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   for_each = var.resource_names_map
 
